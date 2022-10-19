1ddfb201865d287beae31972d1b9be7150bca531
Gulam Ali H <31090457+FreakyAli@users.noreply.github.com>
2022-10-19 20:50:58 +0530

    updated the gitingore file (#1)
    * updated the gitingore file
    
    * feat: basic setup compelete
    
    Co-authored-by: Gulam Ali Hakim <jimtiyg@Gulams-MacBook-Pro.local>

---
+++ Maui.FreakyView/Maui.FreakyView/Shared/FreakyView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace Maui.FreakyView.Shared
+{
+    public class FreakyView : ContentView
+    {
+
+    }
+}
+
