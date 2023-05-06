6ed73dcacbcbc5b6c9bf3d3679c63cbd5a162bec
FreakyAli <31090457+FreakyAli@users.noreply.github.com>
2023-05-07 03:41:47 +0530

    fix: android basic setup for freakyview

--- Maui.FreakyControls/FreakyView/Platforms/Android/FreakyViewHandler.android.cs
+++ Maui.FreakyControls/FreakyView/Platforms/Android/FreakyViewHandler.android.cs
@@ -3,5 +3,21 @@ namespace Maui.FreakyControls;
 
 public partial class FreakyViewHandler
 {
-	
+    private static int blurProcessingDelayMilliseconds = 10;
+
+    public static int BlurProcessingDelayMilliseconds
+    {
+        get => blurProcessingDelayMilliseconds;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(
+                    "The blur processing delay cannot be negative",
+                    nameof(BlurProcessingDelayMilliseconds));
+            }
+
+            blurProcessingDelayMilliseconds = value;
+        }
+    }
 }
